e83d3b3fa02ff851604795046e4b243b0a24af9e
peyush <piyushnawal19.com>
2025-06-17 22:55:27 +0530

    added interview call page

---
+++ src/app/(root)/interview/page.tsx
@@ -0,0 +1,14 @@
+import Agent from '@/components/Agent';
+import React from 'react'
+
+const page = () => {
+    
+  return (
+    <>
+   
+      <Agent userName="YOu" userId="abc2" type="generate" />
+    </>
+  );
+}
+
+export default page
